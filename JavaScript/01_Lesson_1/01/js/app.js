0e6df265f693b56119adf29c612732df86511a03
Iryna Tiutiun <tiutiuniryna@gmail.com>
2022-10-29 20:54:02 +0200

    task_01 to mark up a page using a document write

---
+++ JavaScript/01_Lesson_1/01/js/app.js
@@ -0,0 +1,10 @@
+document.write(
+  `<div class="container bg-white">
+  <header class="d-flex justify-content-center align-items-center">header 800*200</header>
+  <div class="container d-flex row bg-white">
+  <nav class="d-flex justify-content-center bg-white align-items-center">nav 100*400</nav>
+  <section class="d-flex justify-content-center bg-white align-items-center">section 700*400</section>
+  </div>
+    <footer class="d-flex justify-content-center align-items-center">footer 800*200</footer>
+    </div>`
+);

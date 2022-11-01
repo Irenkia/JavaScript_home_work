98100c8fff002eb7c4ce0260dae56651b128c4f6
Iryna Tiutiun <tiutiuniryna@gmail.com>
2022-11-01 16:03:32 +0100

    task_04 functions, calculate

---
+++ JavaScript/04_Lesson_4/04/js/app.js
@@ -0,0 +1,103 @@
+// 1 -----------------------------
+// 1 version----------------------
+document.write("<p> 1 version <br/>");
+var add = function (a, b) {
+  if (a == undefined) {
+    a = 0;
+  } else if (b == undefined) {
+    b = 0;
+  }
+  return a + b;
+};
+var subtract = function (a, b) {
+  if (a == undefined) {
+    a = 0;
+  } else if (b == undefined) {
+    b = 0;
+  }
+  return a - b;
+};
+var multiple = function (a, b) {
+  if (a == undefined) {
+    a = 0;
+  } else if (b == undefined) {
+    b = 0;
+  } else {
+  }
+  return a * b;
+};
+var divide = function (a, b) {
+  if (a == undefined) {
+    a = 0;
+  } else if (b == 0) {
+    document.write(
+      "<p><b style=`color:red;`>ERROR:</b> <em>На ноль делить нельзя, введите другое число</em>"
+    );
+  } else if (b == undefined) {
+    b = 1;
+  }
+  return a / b;
+};
+
+var a;
+var b;
+var sign;
+var result;
+
+do {
+  a = prompt("Введите первое число : ");
+  sign = prompt("Введите знак для операции (+ - * /) : ");
+  b = prompt("Введите второе число : ");
+  var repeat = confirm("Вы хотите повторить операцию ?");
+
+  switch (sign) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiple(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+    default:
+      document.write(
+        "<p><b style='color:red'>" +
+          sign +
+          "</b> - не является знаком арифметической операции."
+      );
+  }
+  if (result != undefined) {
+    document.write("<p>" + a + " " + sign + " " + b + " = " + result);
+  }
+} while (repeat);
+document.write("<hr/>");
+
+// 2 version----------------------
+document.write("<p> 2 version <br/>");
+function calculate(a, b, callBackFunction) {
+  if (arguments.length != 3) {
+    document.write(
+      "<p><b style='color:red;'>ERROR:</b> <em>Требуется три аргумента при вызове.</em><hr/>"
+    );
+  } else {
+    document.write("<p><b style='color:green;'> Правильный вызов функции.</b>");
+    var result2 = callBackFunction(a, b);
+    document.write("<p>" + result2 + "<hr/>");
+  }
+}
+document.write(" 2 + 3 = ");
+calculate(2, 3, add);
+document.write(" 7 - 3 = ");
+calculate(7, 3, subtract);
+document.write(" 4 * 4 = ");
+calculate(4, 4, multiple);
+document.write(" 20 / 5 = ");
+calculate(20, 5, divide);
+document.write("   / 5 = ");
+calculate(5, divide);
+document.write(" 20 / 0 = ");
+calculate(20, 0, divide);

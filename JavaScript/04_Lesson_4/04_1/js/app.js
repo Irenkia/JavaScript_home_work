d1d690ae3ac40d2a2ccee9a0db3fbca8a6362cbe
Iryna Tiutiun <tiutiuniryna@gmail.com>
2022-11-08 21:48:53 +0100

    task 04-1 build a table and display information from an array with function

--- JavaScript/04_Lesson_4/04_1/js/app.js
+++ JavaScript/04_Lesson_4/04_1/js/app.js
@@ -1,11 +1,14 @@
 console.table(countries);
-var htmlString = "";
-for (var countrie of countries) {
-  htmlString += `<tr>
+function getCountries(countries) {
+  var htmlString = "";
+  for (var countrie of countries) {
+    htmlString += `<tr>
     <td class="text-center">${countrie.name.official}</td>
     <td class="text-center">${countrie.region}</td>
     <td class="text-center">${countrie.population}</td>
-    <td class="d-flex justify-content-center"><img src=${countrie.flags.png} class="img-thumbnail w-25"></img></td>
+    <td class="d-flex justify-content-center"><img src=${countrie.flags.png} alt="flag" class="img-thumbnail w-25"></img></td>
   </td>`;
+  }
+  document.getElementById("countries").innerHTML = htmlString;
 }
-document.getElementById("countries").innerHTML = htmlString;
+getCountries(countries);
